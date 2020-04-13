5247ec31fa1434347a2ff94c3345997d573bf9d7
Kav Singh <flakisms@gmail.com>
2020-04-13 14:29:17 +0200

    remove stylelint order plugin from config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   processors: ["stylelint-processor-styled-components"],
-  plugins: ["stylelint-order"],
   extends: [
     "stylelint-config-standard",
     "stylelint-config-styled-components",
