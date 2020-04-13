59c2f6ab3281e035ad5cef36c044463366ad0760
Kav Singh <flakisms@gmail.com>
2020-04-13 17:30:08 +0200

    continuous color interpolation

--- src/components/ChipBall.tsx
+++ src/components/ChipBall.tsx
@@ -1,50 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 import { observer } from "mobx-react";
-import { FCWithoutChildren } from "../types/component";
+
+import { getColorScaler } from "../lib/util/color";
 import { useStore } from "../state/store";
 
-const colors = [
-  "rgb(0,0,255)", // Blue
-  "rgb(21,1,233)",
-  "rgb(42,3,211)",
-  "rgb(63,4,189)",
-  "rgb(84,5,167)",
-  "rgb(105,6,145)",
-  "rgb(127,8,124)",
-  "rgb(148,9,102)",
-  "rgb(169,10,80)",
-  "rgb(190,11,58)",
-  "rgb(211,13,36)",
-  "rgb(232,14,14)", // Red
-  "rgb(211,13,13)",
-  "rgb(190,11,11)",
-  "rgb(169,10,10)",
-  "rgb(148,9,9)",
-  "rgb(127,8,8)",
-  "rgb(105,6,6)",
-  "rgb(84,5,5)",
-  "rgb(63,4,4)",
-  "rgb(42,3,3)",
-  "rgb(21,1,1)",
-  "rgb(0,0,0)", // Black
-];
+import type { FCWithoutChildren } from "../types/component";
 
 const ChipBallComponent: FCWithoutChildren<{
   chipCount: number;
 }> = ({ chipCount }) => {
   const store = useStore();
 
-  const ratio = chipCount / (store.data.table?.maxBetChipCount || 1000);
-  const color = colors[Math.floor(ratio * colors.length)];
-  const size = Math.floor(ratio * 80) + 20;
-
   return (
     <Container>
       {chipCount && (
-        <Chip color={color} size={size}>
+        <Ball ratio={chipCount / (store.data.table?.maxBetChipCount || 1000)}>
           {chipCount}
-        </Chip>
+        </Ball>
       )}
     </Container>
   );
@@ -62,14 +35,15 @@ const Container = styled.div`
   background-color: transparent;
 `;
 
-const Chip = styled.div<{ color: string; size: number }>`
+const Ball = styled.div<{ ratio: number }>`
   position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
-  width: ${({ size }) => size}px;
-  height: ${({ size }) => size}px;
+  width: ${({ ratio }) => Math.floor(ratio * 80) + 20}px;
+  height: ${({ ratio }) => Math.floor(ratio * 80) + 20}px;
   border-radius: 50%;
-  color: ${({ theme }) => theme.colors.chipValue};
-  background-color: ${({ color }) => color};
+  color: ${({ theme }) => theme.colors.chipValueText};
+  background-color: ${({ theme, ratio }) =>
+    getColorScaler(theme.colors.chipValueScale)(ratio)};
 `;
