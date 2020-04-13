61b64946ba0c61518e6943a0c9df289ec2705b12
Kav Singh <flakisms@gmail.com>
2020-04-13 14:28:04 +0200

    remove stylelint order rules that were being flagrantly ignored :)

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,13 +1,9 @@
 module.exports = {
   processors: ["stylelint-processor-styled-components"],
-  plugins: ["stylelint-order", "stylelint-config-rational-order/plugin"],
+  plugins: ["stylelint-order"],
   extends: [
     "stylelint-config-standard",
     "stylelint-config-styled-components",
     "stylelint-prettier/recommended",
   ],
-  rules: {
-    "order/properties-order": [],
-    "plugin/rational-order": [true, { "border-in-box-model": true }],
-  },
 };
